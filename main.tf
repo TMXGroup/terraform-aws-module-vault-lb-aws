aa3d12743a6e548c58bb7362cdb04e8cc0ae7a70
Ryan McCaul <Ryan.McCaul@tmx.com>
2019-01-30 12:11:04 -0500

    testing server side encryption rule

--- main.tf
+++ main.tf
@@ -75,6 +75,14 @@ resource "aws_s3_bucket" "vault_lb_access_logs" {
 
   force_destroy = true
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+
   policy = <<POLICY
 {
   "Id": "Policy",
