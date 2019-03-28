bfa7731ed205581578ec7e1394db8640b44b8e02
121Toscana\Brett <brett.jackson@tmx.com>
2019-03-28 14:56:46 -0400

    Revert "corrected port 80 redirect ARN reference"
    This reverts commit bb8efe9e5972368b0ec8c844540c1aeef0ac1667.

--- main.tf
+++ main.tf
@@ -244,7 +244,7 @@ resource "aws_lb_target_group" "vault_http_8200" {
 resource "aws_lb_listener" "vault_80" {
   count = "${var.create && !var.use_lb_cert ? 1 : 0}"
 
-  load_balancer_arn = "${aws_lb.vault.arn}"
+  listener_arn = "${aws_lb.vault.arn}"
   port              = "80"
   protocol          = "HTTP"
 
